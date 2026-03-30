150493b2f25ef4a376e1f2603d0850d9daac7939
YukivWu <wu.yuqing@wustl.edu>
2026-03-30 11:34:00 -0500

    bump version to 0.1.2 and prepare PyPI release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="SnakeHichipTF",
-    version="0.1.0",
+    version="0.1.2",
     description="A Snakemake pipeline for HiChIP data analysis",
     author="Yidan Sun",
     author_email="syidan@wustl.edu",
--- snakehichiptf/_version.py
+++ snakehichiptf/_version.py
@@ -2,4 +2,4 @@
 SnakeHichipTF: End-to-End framework to integrate HiChIP data analysis and TF binding analysis.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.2"
