9d0e205884a82000623c9e99a333d260ee5bfcb9
YukivWu <wu.yuqing@wustl.edu>
2026-03-30 12:06:59 -0500

    fix workflow permissions

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="SnakeHichipTF",
-    version="0.1.2",
+    version="0.1.3",
     description="A Snakemake pipeline for HiChIP data analysis",
     author="Yidan Sun",
     author_email="syidan@wustl.edu",
--- snakehichiptf/workflows/__init__.py
+++ snakehichiptf/workflows/__init__.py
@@ -2,4 +2,4 @@
 HiChIP Snake: A Snakemake pipeline for HiChIP data analysis.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.3"
